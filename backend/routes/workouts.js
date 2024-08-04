273f696e0cb1d913b94b8f3f8ad815495460c984
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-04 15:19:42 +0545

    workout routing with get post put delete method

---
+++ backend/routes/workouts.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const {
+    createWorkout,
+    getWorkouts,
+    updateWorkout,
+    deleteWorkout,
+} = require('../controllers/workoutController')
+
+const router = express.Router()
+
+router.get('/', getWorkouts)
+
+router.get('/:id',createWorkout)
+
+router.post('/', createWorkout)
+
+router.delete('/:id',deleteWorkout)
+
+router.patch('/:id', updateWorkout)
+
+module.exports = router

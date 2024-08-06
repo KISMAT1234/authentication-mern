dd67a36f66952658a4f337f13412181702630693
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-06 18:14:35 +0545

    jwt token function created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,13 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    jwt.sign({
+      _id},
+      process.env.JWT_SECRET,
+      { expiresIn : '3d'}
+    )
+}
 
 // login a user
 const loginUser = async (req, res) => {
@@ -9,10 +18,13 @@ const loginUser = async (req, res) => {
 const signupUser = async (req, res) => {
     console.log(req.body,'req-body')
     const {email, password} = req.body
+
+    
     console.log(email,password)
 
     try {
       const user = await User.signup(email, password)
+
   
       res.status(200).json({email, user})
     } catch (error) {
